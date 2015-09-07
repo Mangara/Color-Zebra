7c17d0abbdbd815f1163682ce6284af191e3a0eb
Sander Verdonschot <sander.verdonschot@gmail.com>
2015-09-07 14:51:13 -0400

    Correctly set initial colormap.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,19 +1,14 @@
 (function( ColorZebra, $, undefined ) {
+    var INITIAL_COLORMAP = ColorZebra.colorMaps['Lake'];
+    
     ColorZebra.main = function() {
         // Handle on-load stuff
         $(document).ready(function() {
             // Prepare our preview panel
             ColorZebra.mainPreview = new ColorZebra.Preview($('#preview')[0]);
+            ColorZebra.mainPreview.setColorMap(INITIAL_COLORMAP);
             ColorZebra.mainPreview.maximize();
             ColorZebra.mainPreview.draw();
-            
-            /*ColorZebra.spline = new ColorZebra.SplineCanvas($('#spline')[0]);
-            ColorZebra.spline.maximize();
-            ColorZebra.spline.draw();
-            
-            var s = new ColorZebra.LinearSpline([ [12.973, 47.505, -64.705], [97.139, -21.558, 94.477] ]);
-            
-            ColorZebra.Color.test();*/
             
             fillDropdown();
             assignActionHandlers();
@@ -25,7 +20,7 @@
         var select = $('<select>');
         $.each(ColorZebra.colorMaps, function(colorMapName, colorMap) {
             select.append(
-                $('<option></option>').html(colorMapName) /*.val(colorMap)*/
+                $('<option' + (colorMap === INITIAL_COLORMAP ? ' selected' : '') + '></option>').html(colorMapName)
             );
         });
         $('#colormap-select-container').append('<select id="colormap-select">' + select.html() + '</select>');
