b0308d1734f42300fe9eaba1491d1a29dfc7b8a4
Mangara <sander.verdonschot@gmail.com>
2018-03-10 22:51:21 -0500

    Split drawing on the different canvases

--- dev/js/main.js
+++ dev/js/main.js
@@ -275,16 +275,18 @@
 
             updateWidgetBackground(widget);
             updateColorMapFromEditor();
-            updateColorControls();
 
             if ($(this).is(':first-child')) {
                 // The lightness changed
+                updateLightnessControls();
                 updateButtonsEnabledState();
 
                 var newVal = parseInt($(this).val());
 
                 widget.prev().children("input[type=number]").first().attr('max', newVal - 1);
                 widget.next().children("input[type=number]").first().attr('min', newVal + 1);
+            } else {
+                updateABControls();
             }
         });
         
@@ -354,11 +356,31 @@
     }
     
     function updateColorControls() {
+        updateLightnessControls();
+    }
+
+    function updateLightnessControls() {
         var selectedWidget = getSelectedWidget();
         var color = getWidgetColor(selectedWidget);
         
         updateLightnessSlider(selectedWidget, color);
-        updateColorCanvas(color);
+
+        var bgCanvas = $('#abBackground')[0];
+        drawColorCanvasBackground(bgCanvas.getContext("2d"), color[0], bgCanvas.width, bgCanvas.height);
+
+        updateABControls();
+    }
+
+    function updateABControls() {
+        var color = getWidgetColor(getSelectedWidget());
+        var canvas = $('#abControl')[0];
+        var context = canvas.getContext("2d"),
+            width = canvas.width,
+            height = canvas.height;
+
+        context.clearRect(0, 0, width, height);
+        drawColorCanvasCurve(context, color, width, height);
+        drawColorCanvasIndicator(context, color, width, height);
     }
     
     function updateLightnessSlider(selectedWidget, color) {
@@ -387,18 +409,6 @@
         }
 
         $("#dynamic").text("#lightness::-webkit-slider-runnable-track { " + rule + " }");
-    }
-    
-    function updateColorCanvas(color) {
-        var canvas = $('#abControl')[0];
-        var context = canvas.getContext("2d"),
-            x, y,
-            width = canvas.width,
-            height = canvas.height;
-
-        drawColorCanvasBackground(context, color[0], width, height);
-        drawColorCanvasCurve(context, color, width, height);
-        drawColorCanvasIndicator(context, color, width, height);
     }
     
     var minAB = -128;
